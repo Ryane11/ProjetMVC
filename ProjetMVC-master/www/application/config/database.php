76300d38f471491672662983c66a77e48e31333d
Ryane11 <55444750+Ryane11@users.noreply.github.com>
2020-03-03 16:50:37 +0100

    Add files via upload

---
+++ ProjetMVC-master/www/application/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Database configuration settings used by PDO.
+ */
+
+$config['dsn']      = 'mysql:host=localhost;dbname=climbing';
+$config['password'] = 'root';
+$config['user']     = 'root';
